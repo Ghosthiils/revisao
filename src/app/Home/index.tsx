d923ff75d8d6af5f17500faa79c191f2b9f325f3
Luiz Fernando <luizfranca35mc@gmail.com>
2026-05-06 22:04:39 -0300

    App

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,20 +1,26 @@
-
-import { Text, View, StyleSheet } from 'react-native';
-import  { Button } from "@components/Button";
-
+import { Text, View, StyleSheet } from 'react-native'
+import Button from "@/components/Button";
+import Input from "@/components/Input";
 
 export default function App() {
-  
   return (
-    <View style={[styles.container]}>
+    <View style={styles.container}>
+      <Input placeholder='' />
+      <Input readOnly />
+      <Button text="+"
+        idButton={1}
+        isAtivado
+        activeOpacity={0.4} />
 
-    <Text style={[styles.texto, ]}>Hello, World!
+      <Button text="+"
+        idButton={3}
+        isAtivado={false}
+        activeOpacity={0.1} />
 
-    </Text>
-    <button>
-
-    </button>
-
+      <Button text="-"
+        idButton={1}
+        isAtivado={true}
+        activeOpacity={0.4} />
     </View>
   );
 }
@@ -22,15 +28,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-     backgroundColor: 'tomato',
     alignItems: 'center',
     justifyContent: 'center'
   },
   texto: {
     fontSize: 24,
     backgroundColor: 'tomato',
-    fontWeight: '700',
-      color: 'white',
+    fontWeight: 700,
+    color: '#f2f2f2'
+  },
+  button: {
+    padding: 16,
+    height: 24,
+    paddingHorizontal: 24
   }
 })
-
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,28 @@
+import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
+import { style } from "./style";
+
+type Props = TouchableOpacityProps & {
+    text: string,
+    idButton: number,
+    isAtivado: boolean
+}
+
+export default function Button({ text, idButton, isAtivado, ...rest }: Props) {
+    return (
+        <TouchableOpacity
+            {...rest}
+            style={
+                isAtivado === true
+                    ? [style.button, style.buttonAtivo]
+                    : [style.button, style.buttonDesativado]
+            }
+        // style={style.button}
+        >
+            <Text
+                style={style.textoButton}
+            >
+                {text !== '' ? idButton + ' ' + text : "Botão desconfigurado!"}
+            </Text>
+        </TouchableOpacity >
+    )
+}
